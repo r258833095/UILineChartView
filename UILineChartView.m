e23dabfeff7db0db31eea6844ce8440ca0a8b5f9
r258833095 <ly.bear@me.com>
2013-08-21 21:56:34 +0800

    Update UILineChartView.m

--- UILineChartView.m
+++ UILineChartView.m
@@ -12,84 +12,88 @@
 @synthesize interval, minValue, maxValue;
 @synthesize dictData;
 
+static CGSize contentSize;
 - (id)initWithFrame:(CGRect)frame setContentSize:(CGSize)size
 {
     self = [super initWithFrame:frame];
     if (self) {
-        
-        PCLineChartView* xlineChartView = [[PCLineChartView alloc] initWithFrame:CGRectMake(10,10,self.frame.size.width-20,self.frame.size.height-20)];
-        if (interval) {
-            xlineChartView.interval=interval;
-        }
-        if (minValue) {
-            xlineChartView.minValue=minValue;
-        }
-        if (maxValue) {
-            xlineChartView.maxValue=maxValue;
-        }
-        [xlineChartView setAutoresizingMask:UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight];
-        [self addSubview:xlineChartView];
-    
-        UIScrollView *scrollView=[[UIScrollView alloc]initWithFrame:CGRectMake(60, self.frame.origin.y, self.frame.size.width-100, self.frame.size.height)];
-        scrollView.contentSize = size;
-        [scrollView setBackgroundColor:[UIColor clearColor]];
-        [self addSubview:scrollView];
-        
-        PCLineChartView* lineChartView = [[PCLineChartView alloc] initWithFrame:CGRectMake(10,10,size.width-20,size.height-20)];
-        if (interval) {
-             lineChartView.interval = interval;
-        }
-        if (minValue) {
-            lineChartView.minValue = minValue;
-        }
-        if (maxValue) {
-            lineChartView.maxValue = maxValue;
-        }
-        lineChartView.drawYAxis = NO;
-
-
-        [lineChartView setAutoresizingMask:UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight];
-        [scrollView addSubview:lineChartView];
-        
-        
-        NSMutableArray *components = [NSMutableArray array];
-        for (int i=0; i<[[dictData objectForKey:@"data"] count]; i++)
-        {
-            NSDictionary *point = [[dictData objectForKey:@"data"] objectAtIndex:i];
-            PCLineChartViewComponent *component = [[PCLineChartViewComponent alloc] init];
-            [component setTitle:[point objectForKey:@"title"]];
-            [component setPoints:[point objectForKey:@"data"]];
-            [component setShouldLabelValues:NO];
-            
-            if (i==0)
-            {
-                [component setColour:PCColorBlue];
-            }
-            else if (i==1)
-            {
-                [component setColour:PCColorGreen];
-            }
-            else if (i==2)
-            {
-                [component setColour:PCColorOrange];
-            }
-            else if (i==3)
-            {
-                [component setColour:PCColorRed];
-            }
-            else if (i==4)
-            {
-                [component setColour:PCColorYellow];
-            }
-            
-            [components addObject:component];
-        }
-        [lineChartView setComponents:components];
-        [lineChartView setXLabels:[dictData objectForKey:@"x_labels"]];
+        contentSize = size;
+        self.backgroundColor=[UIColor clearColor];
     }
     return self;
 }
 
-
-
+- (void)drawRect:(CGRect)rect
+{
+    
+    PCLineChartView* xlineChartView = [[PCLineChartView alloc] initWithFrame:CGRectMake(10,10,self.frame.size.width-20,self.frame.size.height-20)];
+    if (interval) {
+        xlineChartView.interval=interval;
+    }
+    if (minValue) {
+        xlineChartView.minValue=minValue;
+    }
+    if (maxValue) {
+        xlineChartView.maxValue=maxValue;
+    }
+    [xlineChartView setAutoresizingMask:UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight];
+    [self addSubview:xlineChartView];
+    
+    UIScrollView *scrollView=[[UIScrollView alloc]initWithFrame:CGRectMake(60, self.frame.origin.y, self.frame.size.width-100, self.frame.size.height)];
+    scrollView.contentSize = contentSize;
+    [scrollView setBackgroundColor:[UIColor clearColor]];
+    [self addSubview:scrollView];
+    
+    PCLineChartView* lineChartView = [[PCLineChartView alloc] initWithFrame:CGRectMake(10,10,contentSize.width-20,contentSize.height-20)];
+    if (interval) {
+        lineChartView.interval = interval;
+    }
+    if (minValue) {
+        lineChartView.minValue = minValue;
+    }
+    if (maxValue) {
+        lineChartView.maxValue = maxValue;
+    }
+    lineChartView.drawYAxis = NO;
+    
+    
+    [lineChartView setAutoresizingMask:UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight];
+    [scrollView addSubview:lineChartView];
+    
+    
+    NSMutableArray *components = [NSMutableArray array];
+    for (int i=0; i<[[dictData objectForKey:@"data"] count]; i++)
+    {
+        NSDictionary *point = [[dictData objectForKey:@"data"] objectAtIndex:i];
+        PCLineChartViewComponent *component = [[PCLineChartViewComponent alloc] init];
+        [component setTitle:[point objectForKey:@"title"]];
+        [component setPoints:[point objectForKey:@"data"]];
+        [component setShouldLabelValues:NO];
+        
+        if (i==0)
+        {
+            [component setColour:PCColorBlue];
+        }
+        else if (i==1)
+        {
+            [component setColour:PCColorGreen];
+        }
+        else if (i==2)
+        {
+            [component setColour:PCColorOrange];
+        }
+        else if (i==3)
+        {
+            [component setColour:PCColorRed];
+        }
+        else if (i==4)
+        {
+            [component setColour:PCColorYellow];
+        }
+        
+        [components addObject:component];
+    }
+    [lineChartView setComponents:components];
+    [lineChartView setXLabels:[dictData objectForKey:@"x_labels"]];
+}
 @end
